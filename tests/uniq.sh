bc80eea3310e1668814aa552b8437caa5df231c8
Ramsey Dow <yesmar@gmail.com>
2019-09-17 10:49:44 -0700

    Patch test/uniq.sh to work on macOS
    test/uniq.sh fails because macOS doesn't have an md5sum command.
    A close approximation is md5 -r. I added logic to test/uniq.sh
    to appropriately set a local md5cmd variable (based on the output
    result of uname -s) for use in the subsequent test command.
    
    Fixes #63

--- tests/uniq.sh
+++ tests/uniq.sh
@@ -2,5 +2,9 @@
 
 set -e
 echo "HAL 9000" | $@ -o tmp/uniq-%n -n 100 -p od -m num 
-test 0 = $(md5sum tmp/uniq-* | sed -e 's/ .*//' | sort | uniq -c | grep -v " 1 " | wc -l)
+case $(uname -s) in
+  Darwin) md5cmd="md5 -r";;
+  *) md5cmd="md5sum";;
+esac
+test 0 = $($md5cmd tmp/uniq-* | sed -e 's/ .*//' | sort | uniq -c | grep -v " 1 " | wc -l)
 rm tmp/uniq-*
