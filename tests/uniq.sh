bb3e5fced67b412fb5d0ed649812d39e81e279a5
Ramsey Dow <yesmar@gmail.com>
2019-09-18 08:26:31 -0700

    Update test/uniq.sh to be BSD-friendly

--- tests/uniq.sh
+++ tests/uniq.sh
@@ -2,9 +2,6 @@
 
 set -e
 echo "HAL 9000" | $@ -o tmp/uniq-%n -n 100 -p od -m num 
-case $(uname -s) in
-  Darwin) md5cmd="md5 -r";;
-  *) md5cmd="md5sum";;
-esac
+md5cmd=$(test -x "$(which md5)" && echo "md5 -r" || echo "md5sum")
 test 0 = $($md5cmd tmp/uniq-* | sed -e 's/ .*//' | sort | uniq -c | grep -v " 1 " | wc -l)
 rm tmp/uniq-*
